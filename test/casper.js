64226fada3fc2fe9b389763632418c91887a624e
zesk8 <zapata.sk8@gmail.com>
2015-03-16 23:26:22 -0600

    Add new test

--- test/casper.js
+++ test/casper.js
@@ -14,3 +14,18 @@ casper.test.begin('Search a city', 1, function (test){
 		test.done();
 	});
 });
+
+casper.test.begin('Test recover password', function (test) {
+	casper.start('http://fiestainn.vincoorbisdev.com/recover.html', function (){
+		this.sendKeys('input.c-gray','example@example.com');
+		this.click('button.form-send');
+	});
+
+	casper.wait(3000, function () {
+		test.assertVisible('.recovery-success', 'Send Message');
+	});
+
+	casper.run(function () {
+		test.done();
+	});
+});
