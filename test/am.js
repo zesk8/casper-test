89cf890c81c82e129c8c78404d0bdd608ed4f56a
zesk8 <zapata.sk8@gmail.com>
2015-03-25 16:27:03 -0600

    Add test folder

---
+++ test/am.js
@@ -0,0 +1,19 @@
+// Require mouse module
+var mouse = require("mouse").create(casper);
+// Set viewport
+casper.options.viewportSize = {width: 1280, height: 800};
+
+casper.test.begin('Test modal', function (test) {
+	casper.start('http://aeromexico.com/es/mx/', function (){
+		this.mouse.click('form#flight input.b-orange');
+	});
+
+	casper.waitForSelector('#m-serv', function () {
+		this.echo('see the modal!');
+    this.capture('modal.png');
+	});
+
+	casper.run(function () {
+		test.done();
+	});
+});
